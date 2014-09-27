50e3f17823c241af7cc72b76f7640608b3b03e4c
rvergis <ron.vergis@gmail.com>
2014-09-26 21:37:04 -0700

    wip

---
+++ share_local.sh
@@ -0,0 +1,9 @@
+###############################################################################
+## 
+## Script to zip and share starterpack.sh
+##     
+## Copyright Tinker Academy 2014
+###############################################################################
+PATH_TO_CLASSES=/Users/ron/Dropbox/classes
+
+/Users/ron/Documents/TinkerAcademy/starterpacks
